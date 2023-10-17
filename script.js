4f5e7e345c7d104d7c484cc4adbd8c32796c9f4e
Jaden <jadentr.44@gmail.com>
2023-10-16 21:03:44 -0500

    basics of website

---
+++ script.js
@@ -0,0 +1,28 @@
+// JavaScript
+const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-in");
+if (elementsToFadeInUpOnScroll) {
+  window.addEventListener("scroll", function(event) {
+    elementsToFadeInUpOnScroll.forEach(function(element) {
+      if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
+        element.classList.add("fade-in-up");
+      } else {
+        // element.classList.remove("fade-in-up");
+      }
+    });
+  });
+}
+
+const navLis = document.querySelectorAll('.link')
+navLis.forEach(e=>{
+  e.addEventListener('mouseover', function(){
+
+    e.children[0].classList.add("visible")
+    e.children[0].classList.remove("invisible")
+  })
+  e.addEventListener('mouseout', function(){
+
+    e.children[0].classList.remove("visible")
+    e.children[0].classList.add("invisible")
+  })
+
+})
